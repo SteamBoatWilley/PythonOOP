4adaf1828c32051895a6486e60c5ad5c31afa6ea
Kevin Gerovac <kevintgerovac@gmail.com>
2023-03-16 22:59:58 -0500

    Make Self file for better understanding

---
+++ Self.py
@@ -0,0 +1,9 @@
+class Employee:
+    def employeeDetails(self):
+        self.name = "Matt"
+        print(self.name)
+        self.age = 30
+        print(self.age)
+
+employee = Employee()
+employee.employeeDetails()
